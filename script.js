4a4fd9c1dabe436d73948ec8d5fecbd4a2f9767a
Xero <juliuscaesar.caesar37@gmail.com>
2021-05-30 20:55:48 +0630

    "Third commit"

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ const currentAndTotalTimeTag = document.getElementsByClassName(
   "currentAndTotalTime"
 )[0];
 
+const currentProgressTag = document.getElementById("currentProgress");
+
+const playButtonTag = document.getElementsByClassName("playButton")[0];
+
+const pauseButtonTag = document.getElementsByClassName("pauseButton")[0];
+
+const previousButtonTag = document.getElementsByClassName("previousButton")[0];
+
+const nextButtonTag = document.getElementsByClassName("nextButton")[0];
+
 const tracks = [
   { trackId: "music/track1.mp3", title: "December Nya - Lin Nit" },
   { trackId: "music/track2.mp3", title: "Tsaw Ra Ai Tsaw Hkrup Sai - Ann Naw" },
@@ -20,9 +30,8 @@ const tracks = [
 for (let i = 0; i < tracks.length; i++) {
   const trackTag = document.createElement("div");
   trackTag.addEventListener("click", () => {
-    const trackId = tracks[i].trackId;
-    audioTag.src = trackId;
-    audioTag.play();
+    currentPlayingIndex = i;
+    playSong();
   });
   trackTag.classList.add("trackItem");
   const title = (i + 1).toString() + ". " + tracks[i].title;
@@ -30,9 +39,10 @@ for (let i = 0; i < tracks.length; i++) {
   palyLsitContainerTag.append(trackTag);
 }
 
+let duration = 0;
 let durationText = "00:00";
 audioTag.addEventListener("loadeddata", () => {
-  const duration = Math.floor(audioTag.duration);
+  duration = Math.floor(audioTag.duration);
   durationText = creatMinuteAndSecondText(duration);
 });
 
@@ -41,7 +51,13 @@ audioTag.addEventListener("timeupdate", () => {
   const currentTimeText = creatMinuteAndSecondText(currentTime);
   const currentTimeTextAndDurationText = currentTimeText + " / " + durationText;
   currentAndTotalTimeTag.textContent = currentTimeTextAndDurationText;
+  updateCurrentProgress(currentTime);
 });
+
+const updateCurrentProgress = (currentTime) => {
+  const currentProgressWidth = (500 / duration) * currentTime;
+  currentProgressTag.style.width = currentProgressWidth.toString() + "px";
+};
 
 const creatMinuteAndSecondText = (totalSecond) => {
   const minutes = Math.floor(totalSecond / 60);
@@ -50,4 +66,59 @@ const creatMinuteAndSecondText = (totalSecond) => {
   const minutesText = minutes < 10 ? "0" + minutes.toString() : minutes;
   const secondsText = seconds < 10 ? "0" + seconds.toString() : seconds;
   return minutesText + ":" + secondsText;
+};
+
+let currentPlayingIndex = 0;
+let isPlaying = false;
+playButtonTag.addEventListener("click", () => {
+  isPlaying = true;
+  const currentTime = Math.floor(audioTag.currentTime);
+  if (currentTime === 0) {
+    playSong();
+  } else {
+    audioTag.play();
+    updatePlayAndPauseButton();
+  }
+});
+
+pauseButtonTag.addEventListener("click", () => {
+  isPlaying = false;
+  audioTag.pause();
+  updatePlayAndPauseButton();
+});
+
+previousButtonTag.addEventListener("click", () => {
+  if (currentPlayingIndex === 0) {
+    currentPlayingIndex = tracks.length - 1;
+  } else {
+    currentPlayingIndex -= 1;
+  }
+  playSong();
+});
+
+nextButtonTag.addEventListener("click", () => {
+  if (currentPlayingIndex === tracks.length - 1) {
+    currentPlayingIndex = 0;
+  } else {
+    currentPlayingIndex += 1;
+  }
+  playSong();
+});
+
+const playSong = () => {
+  const songIdToPlay = tracks[currentPlayingIndex].trackId;
+  audioTag.src = songIdToPlay;
+  audioTag.play();
+  isPlaying = true;
+  updatePlayAndPauseButton();
+};
+
+const updatePlayAndPauseButton = () => {
+  if (isPlaying) {
+    playButtonTag.style.display = "none";
+    pauseButtonTag.style.display = "inline";
+  } else {
+    playButtonTag.style.display = "inline";
+    pauseButtonTag.style.display = "none";
+  }
 };
