e7ee2c47364fe6235fd49bb0259eb14b37d09bc6
Xero <juliuscaesar.caesar37@gmail.com>
2021-05-30 11:43:53 +0630

    second commit

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const palyLsitContainerTag =
 
 const audioTag = document.getElementsByClassName("audioTag")[0];
 
+const currentAndTotalTimeTag = document.getElementsByClassName(
+  "currentAndTotalTime"
+)[0];
+
 const tracks = [
   { trackId: "music/track1.mp3", title: "December Nya - Lin Nit" },
   { trackId: "music/track2.mp3", title: "Tsaw Ra Ai Tsaw Hkrup Sai - Ann Naw" },
@@ -25,3 +29,25 @@ for (let i = 0; i < tracks.length; i++) {
   trackTag.textContent = title;
   palyLsitContainerTag.append(trackTag);
 }
+
+let durationText = "00:00";
+audioTag.addEventListener("loadeddata", () => {
+  const duration = Math.floor(audioTag.duration);
+  durationText = creatMinuteAndSecondText(duration);
+});
+
+audioTag.addEventListener("timeupdate", () => {
+  const currentTime = Math.floor(audioTag.currentTime);
+  const currentTimeText = creatMinuteAndSecondText(currentTime);
+  const currentTimeTextAndDurationText = currentTimeText + " / " + durationText;
+  currentAndTotalTimeTag.textContent = currentTimeTextAndDurationText;
+});
+
+const creatMinuteAndSecondText = (totalSecond) => {
+  const minutes = Math.floor(totalSecond / 60);
+  const seconds = totalSecond % 60;
+
+  const minutesText = minutes < 10 ? "0" + minutes.toString() : minutes;
+  const secondsText = seconds < 10 ? "0" + seconds.toString() : seconds;
+  return minutesText + ":" + secondsText;
+};
